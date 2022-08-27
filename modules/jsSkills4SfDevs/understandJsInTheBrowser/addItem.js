f343f71972c015df1cc367c89556084b7f3cd97c
jenigamracy <jeni.gamracy@gmail.com>
2022-08-27 13:16:21 -0600

    Only add item if filled, then clear field afterwards.

--- modules/jsSkills4SfDevs/understandJsInTheBrowser/addItem.js
+++ modules/jsSkills4SfDevs/understandJsInTheBrowser/addItem.js
@@ -1,3 +1,5 @@
+// original exercise - https://jsbin.com/kejonuq/26/edit?html,js,output
+
 // reach into DOM and get the button, 
 // input, and button
 let button = document.getElementById("submit");
@@ -6,18 +8,22 @@ let theList = document.getElementById("thelist");
 
 //Modify DOM by adding click to the button
 button.addEventListener("click", function(event){
-  
-    // turn text input value into HTML text node
-    let textNode = document.createTextNode(newItem.value);
-  
- 
-    //create an li DOM node to insert into the list
-    let newListItem = document.createElement("li");
-  
-    //insert text node into new li node
-    newListItem.appendChild(textNode);
-  
-    //insert li into the ul list
-    theList.appendChild(newListItem);
-  
+    // Only add the item to the list if it's filled out
+    if (newItem.value !== "") {
+        // turn text input value into HTML text node
+        let textNode = document.createTextNode(newItem.value);
+      
+     
+        //create an li DOM node to insert into the list
+        let newListItem = document.createElement("li");
+      
+        //insert text node into new li node
+        newListItem.appendChild(textNode);
+      
+        //insert li into the ul list
+        theList.appendChild(newListItem);
+      
+        // Reset the field
+        newItem.value = "";
+    }
 });
