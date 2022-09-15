9b9f26fc45772d807339cbc3ca35737c1097c404
jenigamracy <jeni.gamracy@gmail.com>
2022-09-15 09:57:38 -0600

    Unit 7: Challenge
    Able to save/retrieve items to/from database

--- modules/auraComponentsBasics/take02/challenges/campingListController.js
+++ modules/auraComponentsBasics/take02/challenges/campingListController.js
@@ -2,33 +2,37 @@
     clickCreateItem : function(component, event, helper) {
         console.log('You got here!');
         
-        let validExpense = component.find('itemform').reduce(function (validSoFar, inputCmp) {
+        let validCampingItem = component.find('itemform').reduce(function (validSoFar, inputCmp) {
             // Displays error messages for invalid fields
             inputCmp.showHelpMessageIfInvalid();
             return validSoFar && inputCmp.get('v.validity').valid;
         }, true);
         
-        if(validExpense){
-            console.log('saving the item now');
-            
+        if(validCampingItem){            
+            // Create the new item
             let newItem = JSON.parse(JSON.stringify(component.get("v.newItem")));
             
-            console.log('New item: ' + JSON.stringify(newItem));
-            
-            let allItems = component.get("v.items");
-            allItems.push(newItem); 
-            component.set("v.items", allItems);
-            
-            console.log('All items: ' + JSON.stringify(allItems));
-            
-            console.log('item name is: ' + component.find("v.itemform"));
-            console.log('really???');
-            
-            // reset newItem - is there a better way to do this?
-            component.set("v.newItem.Quantity__c", 0);
-            component.set("v.newItem.Price__c", 0);
-            component.set("v.newItem.Packed__c", false);
-            component.set("v.newItem.Name", '');
+            let newExpense = component.get("v.newExpense");
+            console.log("Create expense: " + JSON.stringify(newItem));
+            helper.createItem(component, newItem);
         }
-    }
+    },
+    
+    // Load campingItems from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        let action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
 })
